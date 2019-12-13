47ff9555d265e9eb8e818e61d48a9a6518de5640
mikaylamahan <mroller21@gmail.com>
2019-12-13 14:47:58 -0700

    Update

--- Javascript HW/DU-DEN-DATA-PT-08-2019-U-C-master-14-Intro-To-JavaScript-Homework-Instructions/14-Intro-To-JavaScript/Homework/Instructions/StarterCode/static/js/app.js
+++ Javascript HW/DU-DEN-DATA-PT-08-2019-U-C-master-14-Intro-To-JavaScript-Homework-Instructions/14-Intro-To-JavaScript/Homework/Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,28 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//select where you will append your table using d3
+var tbody = d3.select("tbody");
+
+//make sure you can loop through the data. create your own variable..can be anything
+data.forEach(function(Sighting) {
+    console.log(Sighting);
+});
+    
+data.forEach(function(Sighting) {
+    console.log(Sighting);
+    var row = tbody.append("tr");
+    Object.entries(Sighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+//filter the data using a listener
+
+var filterBar= d3.select("#form-group")
+
+
+
+
